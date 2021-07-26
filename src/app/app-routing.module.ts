8eccb92d465e15374eb739f0d68c6b4e2294c2e5
Juansecu <juansecoder@hotmail.com>
2021-07-26 13:17:47 -0500

    Routes for TemplatesModule were setted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'templates',
+    loadChildren: () =>
+      import('./templates/templates.module').then((m) => m.TemplatesModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/templates/templates-routing.module.ts
+++ src/app/templates/templates-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicFormsComponent } from './components/basic-forms/basic-forms.component';
+import { DynamicFormsComponent } from './components/dynamic-forms/dynamic-forms.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic-forms',
+        component: BasicFormsComponent,
+      },
+      {
+        path: 'dynamic-forms',
+        component: DynamicFormsComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'basic-forms',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
