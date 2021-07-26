55b98f701e7c49820b2fde73459ef66f1fef74a5
Juansecu <juansecoder@hotmail.com>
2021-07-26 11:44:07 -0500

    SharedModule was added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+
+@NgModule({
+  declarations: [SidemenuComponent],
+  imports: [CommonModule],
+  exports: [SidemenuComponent],
+})
+export class SharedModule {}
