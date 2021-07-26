19f22af7bee000a30ad372e684412ee251ee5f4e
Juansecu <juansecoder@hotmail.com>
2021-07-26 12:46:19 -0500

    Components for ReactivesModule were added

---
+++ src/app/reactives/components/switch-forms/switch-forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-forms',
+  templateUrl: './switch-forms.component.html',
+  styles: [
+  ]
+})
+export class SwitchFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/reactives/reactives.module.ts
+++ src/app/reactives/reactives.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ReactivesRoutingModule } from './reactives-routing.module';
 
+import { BasicFormsComponent } from './components/basic-forms/basic-forms.component';
+import { DynamicFormsComponent } from './components/dynamic-forms/dynamic-forms.component';
+import { SwitchFormsComponent } from './components/switch-forms/switch-forms.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ReactivesRoutingModule
-  ]
+  declarations: [BasicFormsComponent, DynamicFormsComponent, SwitchFormsComponent],
+  imports: [CommonModule, ReactivesRoutingModule],
 })
-export class ReactivesModule { }
+export class ReactivesModule {}
