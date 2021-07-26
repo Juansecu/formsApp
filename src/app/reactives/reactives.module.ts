dc800e209237735f53d2888d59f21811392a4b0c
Juansecu <juansecoder@hotmail.com>
2021-07-26 11:49:57 -0500

    ReactivesModule was added with its own routes

---
+++ src/app/reactives/reactives.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactivesRoutingModule } from './reactives-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactivesRoutingModule
+  ]
+})
+export class ReactivesModule { }
