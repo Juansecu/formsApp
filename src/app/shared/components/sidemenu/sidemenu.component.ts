2c03b58b0fbe323928244bfd65616b940e6e2243
Juansecu <juansecoder@hotmail.com>
2021-07-27 08:33:57 -0500

    Sidemenu template was setted

--- src/app/shared/components/sidemenu/sidemenu.component.ts
+++ src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Route } from '../../typings/Route';
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SidemenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SidemenuComponent {
+  reactiveRoutes: Route[] = [
+    {
+      text: 'Basic Forms',
+      route: 'reactive-forms/basic-forms',
+    },
+    {
+      text: 'Dynamic Forms',
+      route: 'reactive-forms/dynamic-forms',
+    },
+    {
+      text: 'Switch Forms',
+      route: 'reactive-forms/switch-forms',
+    },
+  ];
+  templateRoutes: Route[] = [
+    {
+      text: 'Basic Forms',
+      route: 'template-forms/basic-forms',
+    },
+    {
+      text: 'Dynamic Forms',
+      route: 'template-forms/dynamic-forms',
+    },
+    {
+      text: 'Switch Forms',
+      route: 'template-forms/switch-forms',
+    },
+  ];
 }
