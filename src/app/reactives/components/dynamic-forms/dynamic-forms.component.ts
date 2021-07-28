c277794992921f58062594c66aac4826e717efa2
Juansecu <juansecoder@hotmail.com>
2021-07-28 12:13:04 -0500

    Template for DynamicFormsComponent from ReactivesModule was setted

--- src/app/reactives/components/dynamic-forms/dynamic-forms.component.ts
+++ src/app/reactives/components/dynamic-forms/dynamic-forms.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms',
   templateUrl: './dynamic-forms.component.html',
   styles: [],
 })
-export class DynamicFormsComponent implements OnInit {
-  constructor() {}
+export class DynamicFormsComponent {
+  form = this.formBuilder.group({
+    firstName: ['', [Validators.required, Validators.minLength(3)]],
+    lastName: [, [Validators.required, Validators.minLength(3)]],
+  });
 
-  ngOnInit(): void {}
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  checkControl(control: string): boolean | null {
+    return (
+      this.form.controls[control].errors && this.form.controls[control].touched
+    );
+  }
+
+  saveData(): void {
+    console.log(this.form.value);
+  }
 }
