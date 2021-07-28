8252d7a2e82edbe372a946407cdb42734a9ee5d8
Juansecu <juansecoder@hotmail.com>
2021-07-28 14:43:55 -0500

    Functionality for favoriteGames was setted into DynamicFormsComponent from ReactivesModule

--- src/app/reactives/components/dynamic-forms/dynamic-forms.component.ts
+++ src/app/reactives/components/dynamic-forms/dynamic-forms.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms',
@@ -10,14 +15,32 @@ export class DynamicFormsComponent {
   form = this.formBuilder.group({
     firstName: ['', [Validators.required, Validators.minLength(3)]],
     lastName: [, [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.formBuilder.array([], [Validators.required]),
   });
 
+  newFavoriteGame = new FormControl('', Validators.required);
+
+  get favoriteGames(): FormArray {
+    return this.form.get('favoriteGames') as FormArray;
+  }
+
   constructor(private readonly formBuilder: FormBuilder) {}
+
+  addFavoriteGame(): void {
+    if (this.newFavoriteGame.invalid) return;
+
+    this.favoriteGames.push(new FormControl(this.newFavoriteGame.value));
+    this.newFavoriteGame.reset();
+  }
 
   checkControl(control: string): boolean | null {
     return (
       this.form.controls[control].errors && this.form.controls[control].touched
     );
+  }
+
+  removeFavoriteGame(index: number): void {
+    this.favoriteGames.removeAt(index);
   }
 
   saveData(): void {
