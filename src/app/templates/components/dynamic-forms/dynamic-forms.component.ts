e1d6b48effa4804ef5325dcf9d33b1ad85a2778a
Juansecu <juansecoder@hotmail.com>
2021-07-27 21:40:45 -0500

    Template for DynamicFormsComponent from TemplatesModule was setted

--- src/app/templates/components/dynamic-forms/dynamic-forms.component.ts
+++ src/app/templates/components/dynamic-forms/dynamic-forms.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FavoriteGame, Person } from 'src/app/shared/typings/Person';
+
 @Component({
   selector: 'app-dynamic-forms',
   templateUrl: './dynamic-forms.component.html',
   styles: [],
 })
 export class DynamicFormsComponent implements OnInit {
+  favoriteGame!: string;
+
+  person: Person = {
+    firstName: '',
+    lastName: '',
+    favoriteGames: [],
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  addFavoriteGame(): void {
+    const newGame: FavoriteGame = {
+      id: this.person.favoriteGames.length + 1,
+      name: this.favoriteGame,
+    };
+
+    this.person.favoriteGames.push({ ...newGame });
+    this.favoriteGame = '';
+  }
+
+  removeFavoriteGame(index: number): void {
+    this.person.favoriteGames.splice(index, 1);
+  }
+
+  saveData(): void {}
 }
