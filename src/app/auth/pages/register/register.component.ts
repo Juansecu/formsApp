289cd595d96a23352f743f21ec449040b2e41b9f
Juansecu <juansecoder@hotmail.com>
2021-07-29 11:43:43 -0500

    Register form from RegisterComponent was turnded as a reactive form

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup = this.formBuilder.group({
+    email: [
+      '',
+      [Validators.required, Validators.pattern(/[a-z]+@[a-z]+.[a-z]{2,4}/)],
+    ],
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    firstName: ['', [Validators.required, Validators.minLength(3)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)]],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(8), Validators.maxLength(20)],
+    ],
+    verifyPassword: [''],
+  });
 
-  constructor() { }
+  constructor(private readonly formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
